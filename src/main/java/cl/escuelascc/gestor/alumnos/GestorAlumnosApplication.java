29f763172e337d125f40c8958f86cbfc5f4e7bd5
Rody <rody2312@hotmail.com>
2022-02-03 19:45:14 -0300

    Primer commit

---
+++ src/main/java/cl/escuelascc/gestor/alumnos/GestorAlumnosApplication.java
@@ -0,0 +1,13 @@
+package cl.escuelascc.gestor.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorAlumnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/escuelascc/gestor/alumnos/GestorAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.escuelascc.gestor.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
